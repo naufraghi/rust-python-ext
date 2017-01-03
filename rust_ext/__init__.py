e6c3a1f93aab395b32506232c75b601e83ff2592
Matteo Bertini <matteo@naufraghi.net>
2017-01-03 23:53:27 +0100

    Trim trailing spaces

--- rust_ext/__init__.py
+++ rust_ext/__init__.py
@@ -45,14 +45,14 @@ class RustBuildCommand(Command):
         else:
             self.debug_or_release = "--release"
 
-        # Make sure that if pythonXX-sys is used, it builds against the current 
+        # Make sure that if pythonXX-sys is used, it builds against the current
         # executing python interpreter.
         bindir = os.path.dirname(sys.executable)
 
         env = os.environ.copy()
         env.update({
             # disables rust's pkg-config seeking for specified packages,
-            # which causes pythonXX-sys to fall back to detecting the 
+            # which causes pythonXX-sys to fall back to detecting the
             # interpreter from the path.
             "PYTHON_2.7_NO_PKG_CONFIG": "1",
             "PATH":  bindir + os.pathsep + os.environ.get("PATH", "")
